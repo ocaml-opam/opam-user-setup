cd6ce89e5e914415f76faef282efaa4bb95a73ba
Louis Gesbert <louis.gesbert@ocamlpro.com>
2015-01-29 09:49:58 +0900

    emacs: fix error-regexp syntax

--- emacs/emacs.ml
+++ emacs/emacs.ml
@@ -64,8 +64,8 @@ let template_ocaml = {elisp|
 (defun set-ocaml-error-regexp ()
   (set
    'compilation-error-regexp-alist
-   '("[Ff]ile \\(\"\\(.*?\\)\", line \\(-?[0-9]+\\)\\(, characters \\(-?[0-9]+\\)-\\([0-9]+\\)\\)?\\)\\(:\n\\(\\(Warning .*?\\)\\|\\(Error\\)\\):\\)?"
-    2 3 (5 . 6) (9 . 11) 1 (8 compilation-message-face))))
+   (list '("[Ff]ile \\(\"\\(.*?\\)\", line \\(-?[0-9]+\\)\\(, characters \\(-?[0-9]+\\)-\\([0-9]+\\)\\)?\\)\\(:\n\\(\\(Warning .*?\\)\\|\\(Error\\)\\):\\)?"
+    2 3 (5 . 6) (9 . 11) 1 (8 compilation-message-face)))))
 
 (add-hook 'tuareg-mode-hook 'set-ocaml-error-regexp)
 (add-hook 'ocaml-mode-hook 'set-ocaml-error-regexp)
